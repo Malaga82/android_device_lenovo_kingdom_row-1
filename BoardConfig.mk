a79ab7b94f2d50f1ab616c6da7c6b1f096ee94dc
Michal Chvíla <electrydev@gmail.com>
2016-08-30 21:37:59 +0200

    Import custom dtbTool from qcom/common
    * Taken from cm
    
    Signed-off-by: Michal Chvíla <electrydev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,10 +44,11 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_DTBTOOL_ARGS := -2
+TARGET_CUSTOM_DTBTOOL := dtbTool
 TARGET_KERNEL_ARCH := arm
-BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=480M
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8974
 TARGET_KERNEL_CONFIG := cyanogenmod_k920_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
